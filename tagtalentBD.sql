91ea6f23c320a262eff8eaae73a5fa6929cfd701
SBTR <03sh.bel.tr@gmail.com>
2023-01-25 12:53:31 -0600

    Se cambió el nombre de la BD

--- tagtalentBD.sql
+++ tagtalentBD.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema tagtalentdb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema tagtalentdb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `tagtalentdb` DEFAULT CHARACTER SET utf8 ;
+USE `tagtalentdb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`usuario`
+-- Table `tagtalentdb`.`usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`usuario` (
+CREATE TABLE IF NOT EXISTS `tagtalentdb`.`usuario` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(20) NOT NULL,
   `apellido` VARCHAR(20) NOT NULL,
@@ -34,9 +34,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`categoria`
+-- Table `tagtalentdb`.`categoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`categoria` (
+CREATE TABLE IF NOT EXISTS `tagtalentdb`.`categoria` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
@@ -44,9 +44,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`publicacion`
+-- Table `tagtalentdb`.`publicacion`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`publicacion` (
+CREATE TABLE IF NOT EXISTS `tagtalentdb`.`publicacion` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `ofrece` ENUM('S', 'N') NOT NULL,
   `titulo` VARCHAR(45) NOT NULL,
@@ -63,9 +63,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`reacciones`
+-- Table `tagtalentdb`.`reacciones`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`reacciones` (
+CREATE TABLE IF NOT EXISTS `tagtalentdb`.`reacciones` (
   `usuario_id` INT NOT NULL,
   `publicacion_id` INT NOT NULL,
   PRIMARY KEY (`usuario_id`, `publicacion_id`),
