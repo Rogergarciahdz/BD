1b3f5e52265ce42f48080089ceb40a338c5aa59b
rogegar <rogegarhdz@gmail.com>
2023-02-03 11:26:39 -0600

    .

--- TagtalentApplication/src/main/resources/static/js/login.js
+++ TagtalentApplication/src/main/resources/static/js/login.js
@@ -1,62 +1,66 @@
+
+
 document.getElementById('formTest')
     .addEventListener('submit', function (event) {
         event.preventDefault();
-
-        let datos = localStorage.getItem('Usuarios');
-        let arrayDatos = [];       
-        let emailView = document.getElementById("email_id").value;
-        let contrasenaView = document.getElementById("pssw_id").value;
-        let alertError = document.getElementById("alertError");
-
-        arrayDatos =JSON.parse(datos)? JSON.parse(datos): []; //if ternario
-
-        alertError.style.display = "none" //Quitar mensaje de error
-        alertError.innerHTML = "";
-
-        if (arrayDatos.some((s)=> {return s.correo==emailView && s.password== contrasenaView})) {
-         
-            window.location.replace("http://127.0.0.1:5512/userprofile.html");
-           
-        } else {
-
-            swal("Error", "El correo o contraseña son inválidos", "error");
-            //alertError.style.display="block";
-           // alertError.innerHTML += "El correo o contraseña son inválidos";
-            //correo.style.border="solid red 1px";
-        }
-        /*if (emailView.value.match(emailRegex)==null) {
-            alertError.style.display="block";
-            alertError.innerHTML += "<br>El correo electrónico no es válido.";
-            emailView.style.border="solid red 1px"
-        }else{
-            emailView.style.border="solid green 1px"
-            validos++;
-        }   
-        
-        buscarCorreo(emailView.value, contrasenaView.value);*/
-
-
-    });
-
+let email=document.getElementById("email_id").value
+let pass=document.getElementById("pssw_id").value
+      
+		
+		const form = {
+  			correo:email,
+  			password:pass
+  			
+		};
+		console.log(form);
+		const url ='/api/usuario/';
+		fetch(url, {
+    method: "POST",
+    headers: {
+     'Content-Type': 'application/json',
+    },
+  
+     body: JSON.stringify(form),
     
+  })
+    .then((response) => response.json())
+    .then((form) => {
+      console.log(form);
+      // code here //
+      if (error) {
+        swal("Error", "El correo o contraseña son inválidos", "error");
+      } else {
+         window.location.replace("http://127.0.0.1:5512/userprofile.html");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
 
-    /*function buscarCorreo(correo, contrasena) {
-        for(let index = 0; index < arrayDatos.length; index++){
-            if(correo === arrayDatos[index].correo){
-                usuario = arrayDatos[index]
-                console.log("Encontre el usuario", usuario);
-            }           
-            
-        }  
-        
-        if(usuario.password === contrasena){
-            alert("Inicio de sesión correcto");
-        }
-        else{
-            alertError.style.display="block";
-            alertError.innerHTML += "<br>Usuario y/o contraseña incorrecta";
-            emailView.style.border="solid red 1px"
-            contrasenaView.style.border="solid red 1px"
-        }
-        
-    }*/
+       
